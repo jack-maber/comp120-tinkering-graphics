05987382dc185729ce7e19e383c19aef02eb07a1
jack-maber <jack-maber@live.co.uk>
2016-10-12 17:50:15 +0100

    Added code to create window

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
-import pygame
+import pygame, sys, time
+from pygame.locals import *
+
+pygame.init()
+
+Height = 800
+Width = 800
+
+window = pygame.display.set_mode((Width, Height), 0, 24)
+pygame.display.set_caption('Meme Generator')
+
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
