26f39f57afbf79247c8c7f2208807a113289681b
jack-maber <jm192929@falmouth.ac.uk>
2016-10-25 12:03:01 +0100

    Added new links and changed text location

--- Main.py
+++ Main.py
@@ -2,19 +2,22 @@ import pygame, sys, time
 from pygame.locals import *
 
 pygame.init()
-img = pygame.image.load('BigSmoke.jpg')
-myfont = pygame.font.SysFont("comic", 100)
-Height =600
+img = pygame.image.load('BigSmoke.jpg')  #Imports background image
+myfont = pygame.font.SysFont("comic", 100) #Imports text font
+Height =600 #Sets dimensions of window
 Width =600
 
-window = pygame.display.set_mode((Width, Height), 0, 24)
-pygame.display.set_caption('Meme Generator')
-window.blit(img,(0,0))
+window = pygame.display.set_mode((Width, Height), 0, 24) #Creates window
+pygame.display.set_caption('Meme Generator') #Sets caption for game window
+window.blit(img,(0,0)) #Blits image to created window
+
+
 
 
 label = myfont.render("CJ?", 18, (255,255,255))
-text_rect = label.get_rect(center=(Width/2, Height/2))
+text_rect = label.get_rect(center=(Width/2, 550))
 window.blit(label, text_rect)
+
 
 while True:
     for event in pygame.event.get():
