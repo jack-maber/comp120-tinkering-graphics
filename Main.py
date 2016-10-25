e194bfb65d4b4b8a8be5602dbc5cb314e28b2792
jack-maber <jm192929@falmouth.ac.uk>
2016-10-25 11:00:21 +0100

    Added auto centering

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 img = pygame.image.load('BigSmoke.jpg')
-myfont = pygame.font.SysFont("comic", 60)
+myfont = pygame.font.SysFont("comic", 100)
 Height =600
 Width =600
 
@@ -11,8 +11,10 @@ window = pygame.display.set_mode((Width, Height), 0, 24)
 pygame.display.set_caption('Meme Generator')
 window.blit(img,(0,0))
 
-label = myfont.render("CJ?", 18, (255,255,0))
-window.blit(label, (150, 100))
+
+label = myfont.render("CJ?", 18, (255,255,255))
+text_rect = label.get_rect(center=(Width/2, Height/2))
+window.blit(label, text_rect)
 
 while True:
     for event in pygame.event.get():
